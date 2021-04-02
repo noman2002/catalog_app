19c0fb0e74057161b72b92f2a648068de387eed4
Noman <nomn2002@gmail.com>
2021-04-03 04:32:11 +0530

    fixed animation

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -67,7 +67,11 @@ class _LoginPageState extends State<LoginPage> {
                             changeButton = true;
                           });
                           await Future.delayed(Duration(seconds: 1));
-                          Navigator.pushNamed(context, MyRoutes.homeRoute);
+                          await Navigator.pushNamed(
+                              context, MyRoutes.homeRoute);
+                          setState(() {
+                            changeButton = false;
+                          });
                         },
                         child: AnimatedContainer(
                           duration: Duration(seconds: 1),
