ff99b6715693eff1a569722daf4dcef269577d9f
noman2002 <nomn2002@gmail.com>
2021-04-17 17:06:05 +0530

    day 23 commits

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -14,5 +14,17 @@ class CartModel {
   List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
 
   //get total price
-  num get totalPrice => items.fold(0, (total, current) => total+current.price);
+  num get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+
+//add item
+
+  void add(Item item) {
+    _itemIds.add(item.id);
+  }
+
+//remove items
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+  }
 }
